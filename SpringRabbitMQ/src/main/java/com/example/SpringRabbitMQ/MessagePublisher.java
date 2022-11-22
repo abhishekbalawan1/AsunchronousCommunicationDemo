e8b489b363a0ebc17d3c506edeec272f3e626902
Abhishek Balawan <abhishekbalawan@Abhisheks-MBP.domain.name>
2022-11-22 14:52:23 +0530

    adding repos

---
+++ SpringRabbitMQ/src/main/java/com/example/SpringRabbitMQ/MessagePublisher.java
@@ -0,0 +1,28 @@
+package com.example.SpringRabbitMQ;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class MessagePublisher {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private RabbitTemplate template2;
+
+    @PostMapping("/publish")
+    public String publishMessage(@RequestBody CustomMessage message){
+        message.setMessageId("1234");
+        message.setMessageDate(new Date());
+        template.convertAndSend("message_exchange", "message_routing_key", message);
+        return "message published";
+    }
+}
